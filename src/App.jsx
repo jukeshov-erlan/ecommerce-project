0c44907ebf7d27a39953009b12ee577a58b481fc
jukeshov erlan <jukeshov.erlan@gmail.com>
2025-09-22 12:56:28 +0600

    Create add to cart feature

--- src/App.jsx
+++ src/App.jsx
@@ -11,21 +11,22 @@ import "./App.css";
 function App() {
   const [cart, setCart] = useState([]);
 
-  useEffect(() => {
-    async function fetchAppData() {
-      try {
-        const response = await axios.get("/api/cart-items?expand=product");
-        setCart(response.data);
-      } catch (err) {
-        console.error(err);
-      }
+  async function loadCart() {
+    try {
+      const response = await axios.get("/api/cart-items?expand=product");
+      setCart(response.data);
+    } catch (err) {
+      console.error(err);
     }
-    fetchAppData();
+  }
+
+  useEffect(() => {
+    loadCart();
   }, []);
   return (
     <>
       <Routes>
-        <Route index element={<HomePage cart={cart} />} />
+        <Route index element={<HomePage cart={cart} loadCart={loadCart} />} />
         <Route path="checkout" element={<CheckoutPage cart={cart} />} />
         <Route path="orders" element={<OrdersPage cart={cart} />} />
         <Route
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -4,7 +4,7 @@ import Header from "../../components/Header";
 import ProductsGrid from "./ProductsGrid";
 import "./HomePage.css";
 
-export default function HomePage({ cart }) {
+export default function HomePage({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -21,7 +21,7 @@ export default function HomePage({ cart }) {
       <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
       <Header cart={cart} />
       <div className="home-page">
-        <ProductsGrid products={products} />
+        <ProductsGrid products={products} loadCart={loadCart} />
       </div>
     </>
   );
